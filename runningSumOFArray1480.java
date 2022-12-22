13a0b22eab82cff6b4925e3e73dca887b1b589ff
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2022-12-22 12:16:15 +0530

    1480

--- runningSumOFArray1480.java
+++ runningSumOFArray1480.java
@@ -1,8 +1,17 @@
+import java.util.Arrays;
+
 public class runningSumOFArray1480 {
-    public int[] runningSum(int[] nums) {
-        for(int i = 1; i < nums.length; i++){
-            nums[i] = nums[ i - 1] + nums[i];
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3, 4 };
+        int[] ans = runningSumOFArray1480Son(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    private static int[] runningSumOFArray1480Son(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + nums[i];
         }
         return nums;
     }
+
 }
