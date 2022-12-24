38c51c1d27063bcba08ea11a87e4093436539863
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2022-12-24 13:48:18 +0530

    1365

---
+++ NumbersAreSmallerThanTheCurrentNumber1365.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class NumbersAreSmallerThanTheCurrentNumber1365 {
+    public static void main(String[] args) {
+        int[] nums = { 8, 1, 2, 2, 3 };
+        int[] ans = smallerNumbersThanCurrent(nums);
+        System.out.println(Arrays.toString(ans));
+
+    }
+
+    private static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    count++;
+                }
+                result[i] = count;
+            }
+
+        }
+        return result;
+    }
+}
