cef2adc8ede7a7a06eb52c9123db768884087561
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2022-12-30 23:48:09 +0530

    1295

--- EvenNumOfDigits.java
+++ EvenNumOfDigits.java
@@ -6,19 +6,25 @@ public class EvenNumOfDigits {
     }
 
     private static int FindNumber(int[] nums) {
-        int numEvn = 0;
+        // int numEvn = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if ((countDigit(nums[i]) % 2) == 0) {
-                numEvn++;
-            }
-        }
-        return numEvn;
+        // for (int i = 0; i < nums.length; i++) {
+        // if ((countDigit(nums[i]) % 2) == 0) {
+        // numEvn++;
+        // }
+        // }
+        // return numEvn;
+        int count = 0;
+
+        for (int num : nums)
+            count += String.valueOf(num).length() % 2 == 0 ? 1 : 0;
+
+        return count;
     }
 
-    private static int countDigit(int number) {
+    // private static int countDigit(int number) {
 
-        return (int) Math.floor(Math.log10(number) + 1);
+    // return (int) Math.floor(Math.log10(number) + 1);
 
-    }
+    // }
 }
