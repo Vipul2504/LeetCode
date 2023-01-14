84c163d3120b0fbbc47caee42cdff21194e7fe33
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-14 23:56:26 +0530

    50 done

---
+++ Medium/Spiral_matrix_50.java
@@ -0,0 +1,9 @@
+public class Spiral_Matrix_50 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3}, {4,5,6},{7,8,9}};
+        int[] ans = SpiralMatrix(matrix);
+        
+    }
+
+
+}
