02c3f692b0a35a3a8e0a1bb11613d51de48624a2
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-15 23:10:25 +0530

    2535

---
+++ DifferenceBetweenElementSumandDigitSum2535.java
@@ -0,0 +1,20 @@
+public class DifferenceBetweenElementSumandDigitSum2535 {
+    public static void main(String[] args) {
+        int[] nums = { 4, 23, 21, 5, 4, 23, 5, 6, 32 };
+        int ans = DifferenceBetElemAndDigit(nums);
+        System.out.println(ans);
+    }
+
+    private static int DifferenceBetElemAndDigit(int[] nums) {
+        int esum = 0, dsum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            esum += nums[i];
+            while (nums[i] != 0) {
+                dsum += nums[i] % 10;
+                nums[i] /= 10;
+            }
+        }
+        return Math.abs(dsum - esum);
+    }
+
+}
