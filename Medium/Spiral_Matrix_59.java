6424f8e3e5b7280df07589055cd72ff265d5901f
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-16 12:13:12 +0530

    Spiral-Matrix-2 59 done

---
+++ Medium/Spiral_Matrix_59.java
@@ -0,0 +1,52 @@
+package Medium;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Spiral_Matrix_59 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number :- ");
+            int num = sc.nextInt();
+            int[][] ans = SpiralMatrixII(num);
+            System.out.println(Arrays.deepToString(ans));
+        }
+
+    }
+
+    private static int[][] SpiralMatrixII(int n) {
+        int[][] res = new int[n][n];
+
+        int cur = 1;
+        int rowBegin = 0;
+        int rowEnd = n - 1;
+        int colBegin = 0;
+        int colEnd = n - 1;
+
+        while (cur <= n * n) {
+            int i = rowBegin;
+            int j = colBegin;
+            // left to right
+            for (j = colBegin; j <= colEnd; j++) {
+                res[rowBegin][j] = cur++;
+            }
+            rowBegin++;
+            // top to bot
+            for (i = rowBegin; i <= rowEnd; i++) {
+                res[i][colEnd] = cur++;
+            }
+            colEnd--;
+            // right to left
+            for (j = colEnd; j >= colBegin; j--) {
+                res[rowEnd][j] = cur++;
+            }
+            rowEnd--;
+            // bot to top
+            for (i = rowEnd; i >= rowBegin; i--) {
+                res[i][colBegin] = cur++;
+            }
+            colBegin++;
+        }
+        return res;
+    }
+}
