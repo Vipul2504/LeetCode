e7d8aadcd5a23733500353e2482e05f90eb8411d
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-17 17:11:19 +0530

    238 done

---
+++ Medium/Product_of_Array_Except_Self_238.java
@@ -0,0 +1,28 @@
+package Medium;
+
+import java.util.Arrays;
+
+/**
+ * Product_of_Array_Except_Self_238
+ */
+public class Product_of_Array_Except_Self_238 {
+
+    public static void main(String[] args) {
+        int[] nums = { 4, 5, 1, 8, 2 };
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+
+    private static int[] productExceptSelf(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0, left = 1; i < nums.length; i++) {
+            ans[i] = left;
+            left *= nums[i];
+        }
+        for (int j = nums.length - 1, right = 1; j >= 0; j--) {
+            ans[j] *= right;
+            right *= nums[j];
+        }
+        return ans;
+    }
+
+}
