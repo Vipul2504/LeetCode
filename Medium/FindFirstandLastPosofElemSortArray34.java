f6aeb3fcda937d581a4ea20bf3af1951e9a3aa9e
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-17 17:30:55 +0530

    34

---
+++ Medium/FindFirstandLastPosofElemSortArray34.java
@@ -0,0 +1,52 @@
+package Medium;
+
+import java.util.Arrays;
+
+public class FindFirstandLastPosofElemSortArray34 {
+    public static void main(String[] args) {
+        int nums[] = { 5, 6, 7, 7, 8, 8, 10 }, target = 8;
+        System.out.println(Arrays.toString(SearchRange(nums, target)));
+
+    }
+
+    private static int[] SearchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    private static int findLast(int[] nums, int target) {
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            if (nums[mid] == target)
+                idx = mid;
+        }
+        return idx;
+    }
+
+    private static int findFirst(int[] nums, int target) {
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            if (nums[mid] == target)
+                idx = mid;
+        }
+        return idx;
+    }
+}
