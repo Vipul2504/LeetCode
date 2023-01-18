b2781a695f4c7674d861c34dc648ceb1025bd428
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-18 18:03:02 +0530

    1752-LC GFG

---
+++ CheckIfArrayIsSorted.java
@@ -0,0 +1,29 @@
+public class CheckIfArrayIsSorted {
+    public static void main(String[] args) {
+        int[] nums = { 10, 20, 30, 40, 50 };
+        Boolean GFGans = arraySortedOrNot(nums); // for gfg
+        Boolean LEETans = checkSortandRotate(nums); // for LeetCode
+        System.out.println(GFGans);
+        System.out.println(LEETans);
+    }
+
+    private static Boolean checkSortandRotate(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[(i + 1) % nums.length]) {
+                count++;
+            }
+
+        }
+        return (count <= 1);
+    }
+
+    private static Boolean arraySortedOrNot(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
