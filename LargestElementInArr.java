b6dd27ad8d310f0c31b88cac00420cb8f9fc7c6b
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-18 18:03:20 +0530

    GFG

---
+++ LargestElementInArr.java
@@ -0,0 +1,17 @@
+public class LargestElementInArr {
+    public static void main(String[] args) {
+        int[] nums = { 2, 25, 44, 27, 65, 93, 87, 59, 34, 15 };
+        int ans = LargestElement(nums);
+        System.out.println(ans);
+    }
+
+    private static int LargestElement(int[] nums) {
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        return max;
+    }
+}
