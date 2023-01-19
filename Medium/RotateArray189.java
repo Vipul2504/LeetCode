c7f7dc80ad0dfa95be67ce9b733f1007e9efd3ac
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-20 00:11:51 +0530

    189 modified

--- Medium/RotateArray189.java
+++ Medium/RotateArray189.java
@@ -6,21 +6,12 @@ public class RotateArray189 {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
         int k = 3;
-        System.out.println(Arrays.toString(rotate(arr, k)));
+        rotate(arr, k);
+        System.out.println(Arrays.toString(arr));
+
     }
 
-    private static long[] rotate(int[] arr, int k) {
-        k = k % arr.length;
-        if (k < 0) {
-            k = k + arr.length;
-        }
-        reverse(arr, 0, arr.length - k - 1);
-        reverse(arr, arr.length - k, arr.length - 1);
-        reverse(arr, 0, arr.length - 1);
-        return null;
-    }
-
-    private static void reverse(int[] arr, int left, int right) {
+    public static void reverse(int[] arr, int left, int right) {
         while (left < right) {
             int temp = arr[left];
             arr[left] = arr[right];
@@ -29,4 +20,15 @@ public class RotateArray189 {
             right--;
         }
     }
+
+    public static void rotate(int[] arr, int k) {
+        k = k % arr.length;
+        if (k < 0) {
+            k = k + arr.length;
+        }
+        reverse(arr, 0, arr.length - k - 1);
+        reverse(arr, arr.length - k, arr.length - 1);
+        reverse(arr, 0, arr.length - 1);
+    }
+
 }
