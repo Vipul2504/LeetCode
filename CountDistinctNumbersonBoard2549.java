bd9bbcde0bcb76e247588d3c1f6696f5a52054a9
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-01-29 13:52:33 +0530

    2549

---
+++ CountDistinctNumbersonBoard2549.java
@@ -0,0 +1,16 @@
+
+public class CountDistinctNumbersonBoard2549 {
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = distinctIntegers(n);
+        System.out.println(ans);
+
+    }
+
+    private static int distinctIntegers(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n - 1;
+    }
+}
