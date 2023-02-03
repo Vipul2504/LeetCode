000ec4da7fd51810194e099264f24785b39b0bce
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-03 22:53:33 +0530

    167

---
+++ Medium/TwoSumII-InputArrayIsSorted167.java
@@ -0,0 +1,29 @@
+package Medium;
+
+/**
+ * TwoSumII-InputArrayIsSorted167
+ */
+public class TwoSumII-InputArrayIsSorted167
+{
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int ans[] = TwoSumII(nums, target);
+    }
+
+    private static int[] TwoSumII(int[] nums, int target) {
+        int lo = 0;
+    int hi = nums.length - 1;
+        while(nums[lo] + nums[hi] != target){
+            if(nums[lo] + nums[hi] < target){
+                lo++;
+            }
+            else{
+                hi--;
+            }
+        }
+        return new int[] {lo + 1, hi + 1};
+    }
+
+}
