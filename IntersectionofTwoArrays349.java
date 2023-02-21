007479a991fddc7e71584c27189feb3e38cc465d
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-21 22:58:11 +0530

    349

---
+++ IntersectionofTwoArrays349.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionofTwoArrays349 {
+    public static void main(String[] args) {
+        int[] nums1 = { 4, 9, 5 };
+        int[] nums2 = { 9, 4, 9, 8, 4 };
+        int[] ans = IntersectionofTwoArrays(nums1, nums2);
+    }
+
+    private static int[] IntersectionofTwoArrays(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
+    }
+}
