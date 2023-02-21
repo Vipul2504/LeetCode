51d4a85682922c9e4bd8eb8f2a7b14cf0593b173
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-21 22:58:32 +0530

    le

--- FindSmallestLetterGreaterThanTarget744.java
+++ FindSmallestLetterGreaterThanTarget744.java
@@ -1,5 +1,6 @@
 public class FindSmallestLetterGreaterThanTarget744 {
     public static void main(String[] args) {
-        char[] letters = {"c","f","j"}, target = "a"
+        String[] letters = { "c", "f", "j" };
+        char target = 'a';
     }
 }
