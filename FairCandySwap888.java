f6d6e254a977a3ba3e3a910f0dca444e820d7930
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-22 23:21:28 +0530

    888

---
+++ FairCandySwap888.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.IntStream;
+
+public class FairCandySwap888 {
+    public static void main(String[] args) {
+        int[] aliceSizes = { 1, 1 };
+        int[] bobSizes = { 2, 2 };
+        int[] ans = FairCandySwap(aliceSizes, bobSizes);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    private static int[] FairCandySwap(int[] A, int[] B) {
+        int dif = (IntStream.of(A).sum() - IntStream.of(B).sum()) / 2;
+        HashSet<Integer> S = new HashSet<>();
+        for (int a : A)
+            S.add(a);
+        for (int b : B)
+            if (S.contains(b + dif))
+                return new int[] { b + dif, b };
+        return new int[0];
+    }
+}
