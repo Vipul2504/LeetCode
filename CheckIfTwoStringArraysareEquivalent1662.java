bbc399576b45e932a206cbc1c1d4e3e0f0099f7a
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-02-27 22:19:31 +0530

    1662

---
+++ CheckIfTwoStringArraysareEquivalent1662.java
@@ -0,0 +1,12 @@
+public class CheckIfTwoStringArraysareEquivalent1662 {
+    public static void main(String[] args) {
+        String[] word1 = {"ab","c"};
+        String[] word2 = {"a","bc"};
+        boolean ans = IsEqual(word1,word2);
+        System.out.println(ans);
+    }
+
+    private static boolean IsEqual(String[] word1, String[] word2) {
+        return(String.join("", word1).equals(String.join("", word2)));
+    }
+}
