75d0aeb4af526de31e381568cb39fb2affb053e0
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-03-01 21:46:05 +0530

    628

---
+++ MaximumProductofThreeNumbers628.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class MaximumProductofThreeNumbers628 {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,7,9};
+        int ans = MaximumProductofThreeNumbers(nums);
+        System.out.println(ans);
+    }
+
+    private static int MaximumProductofThreeNumbers(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+       return Math.max(nums[n-1]*nums[n-2]*nums[n-3] , nums[0]*nums[1]*nums[n-1]);
+    }
+}
