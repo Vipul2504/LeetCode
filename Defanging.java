0c66ed337f49cf068f61fa0683610298a8f90464
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-03-08 23:25:37 +0530

    1108

---
+++ Defanging.java
@@ -0,0 +1,11 @@
+public class Defanging {
+    public static void main(String[] args) {
+        String address = "255.100.50.0";
+        String ans = DefangingIpAdd(address);
+        System.out.println(ans);
+    }
+
+    private static String DefangingIpAdd(String address) {
+        return address.replace(".", "[.]");
+    }
+}
