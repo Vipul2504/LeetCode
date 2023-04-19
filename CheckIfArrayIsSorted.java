a2b354f1a470aae3fd43185c4cdb5cd07e4e5773
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-04-19 23:19:12 +0530

    M

--- CheckIfArrayIsSorted.java
+++ CheckIfArrayIsSorted.java
@@ -4,7 +4,7 @@ public class CheckIfArrayIsSorted {
         Boolean GFGans = arraySortedOrNot(nums); // for gfg
         Boolean LEETans = checkSortandRotate(nums); // for LeetCode
         System.out.println(GFGans);
-        System.out.println(LEETans);
+
     }
 
     private static Boolean checkSortandRotate(int[] nums) {
