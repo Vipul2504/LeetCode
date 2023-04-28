5a61d2678d56bcd1ff7e27992c875153a1f6000c
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-04-28 23:43:58 +0530

    M

--- SentenceIsPanagram.java
+++ SentenceIsPanagram.java
@@ -9,6 +9,7 @@ public class SentenceIsPanagram {
         String sentence = "abcdefghijklmnopqrstuvwxyz";
         boolean res = CheckIssentenceIspanagram(sentence);
         System.out.println(res);
+        System.out.println(res);
     }
 
     private static boolean CheckIssentenceIspanagram(String sentence) {
