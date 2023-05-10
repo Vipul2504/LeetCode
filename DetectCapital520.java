564e6c0b0ea7ae6ffaf2316b5ab22fe6b1094622
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2023-05-10 22:59:12 +0530

    M

--- DetectCapital520.java
+++ DetectCapital520.java
@@ -3,7 +3,6 @@ public class DetectCapital520 {
         String word = "USA";
         Boolean Ans = DetectCapital(word);
         System.out.println(Ans);
-        System.out.println(Ans);
     }
 
     private static Boolean DetectCapital(String word) {
